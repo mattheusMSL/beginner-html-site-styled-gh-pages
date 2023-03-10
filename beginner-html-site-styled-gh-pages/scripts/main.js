cb7a01e5fc8008bb3b3292fd0943d426db6bbb9a
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2023-03-09 22:10:11 -0300

    localStorage name

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,25 +1,50 @@
-const myHeading = document.querySelector("h1");
-
-myHeading.textContent = "Hello World!";
-
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello World!";
 
 // function showMessage(text){
-
-//     text = window.alert("Hello world!");
+//     text = alert("Hello world!");
 //     return text;
 // }
 
 // showMessage();
 
-const myImage = document.querySelector('img');
+/* changing the image by using the arrow function and using onclick function*/ 
 
-myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/firefox-icon.png"){
-        myImage.setAttribute("src", "images/tomorrowland.png");
-        myImage.style.height = '256px';
-        myImage.style.width = "256px";
-    }else{
-        myImage.setAttribute("src", "images/firefox-icon.png");
-    }
+// const myImage = document.querySelector('img');
+
+// myImage.onclick = () => {
+//     const mySrc = myImage.getAttribute("src");
+//     if (mySrc === "images/firefox-icon.png"){
+//         myImage.setAttribute("src", "images/tomorrowland.png");
+//         myImage.style.height = '256px';
+//         myImage.style.width = "256px";
+//     }else{
+//         myImage.setAttribute("src", "images/firefox-icon.png");
+//     }
+// }
+
+// /* changing user */ 
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+    const myName = prompt("Please enter your name!");
+    localStorage.setItem("name". myName);
+    myHeading.textContent = `Greeting from Mozilla dear ${myName}!`;
 }
+  
+if(!localStorage.getItem("name")){
+    setUserName();
+  }else{
+      const storedName = localStorage.getItem("name");
+      myHeading.textContent = `Greeting from Mozilla dear ${storedName}`;
+  
+  }
+  
+  myButton.onclick = () => {
+      setUserName();
+  }
+
+
+
