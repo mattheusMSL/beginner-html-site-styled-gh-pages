71f1fb50d11f52b57db54fb95517e88a0f05a3f0
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2023-03-09 21:12:36 -0300

    adding arrow function to change the image by using onclick function

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -3,11 +3,23 @@ const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello World!";
 
 
-function showMessage(text){
+// function showMessage(text){
 
-    text = window.alert("Hello world!");
-    return text;
+//     text = window.alert("Hello world!");
+//     return text;
+// }
+
+// showMessage();
+
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png"){
+        myImage.setAttribute("src", "images/tomorrowland.png");
+        myImage.style.height = '256px';
+        myImage.style.width = "256px";
+    }else{
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
 }
-
-
-showMessage();
